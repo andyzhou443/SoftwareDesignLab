c385bfe996aaf73efbc39d1022fe97d8cf552de4
Andy Zhou <andyzhou443@gmail.com>
2021-03-09 20:03:12 -0500

    Updated AssignmentTwo
    Updated AssignmentTwo

---
+++ AssignmentTwo/SkeeBall.java
@@ -0,0 +1,21 @@
+/**
+ * @author Andy Zhou
+ * 
+ */
+
+ /*
+ 
+ */
+import java.util.Scanner; //Scanner for user input
+
+public class SkeeBall{
+
+
+    System.out.print("");
+
+    static final int MAX_PLAYS = 8;
+
+    void displayStats(){
+
+    }
+}
