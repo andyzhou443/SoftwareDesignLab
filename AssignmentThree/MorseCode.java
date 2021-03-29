e0fd3c8208325010a2029d68f876a6a1b0ba977b
Andy Zhou <andyzhou443@gmail.com>
2021-03-28 21:34:42 -0400

    Completed Assignment3

--- AssignmentThree/MorseCode.java
+++ AssignmentThree/MorseCode.java
@@ -5,19 +5,24 @@
 *@version 1.0   3/26/2021
 */
 
+/**
+ * Class contains methods for setters and getters.
+ * Class also contains non-default contructor that sets user input for character and encoding to that of the constructor
+ */
 public class MorseCode{
     char character;
     String encoding;
 
      /** 
      * public MorseCode(char characterInput, String encodingInput) is the class contructor with parameters from user input
-     * @param characterInput sets name to nameInput 
-     * @param encodingInput sets year_of_birth to year_of_Birth_Input
-     * ADD THAT IT CHECK
+     * @param characterInput sets user input character to the one stored in MorseCode constructior
+     * @param encodingInput sets user input encoding to the one stored in MorseCode constructior
+     * @throws Exception will throw an Exception if a character input is not supported by morse code
+     * public MorseCode only accept character values of ASCII codes between 32 and 90 and an encoding value that cannot be NULL and of leangth at least one. If criteria is not met, an Exception is thrown.
      */
-    public MorseCode(char characterInput, String encodingInput){
-        if (characterInput < 32 || characterInput > 90 || encodingInput != NULL || encodingInput.length < 1){
-            throw new Exception ("The character " + characterInput + "is not supported Morse character");
+    public MorseCode(char characterInput, String encodingInput)throws Exception{
+        if(characterInput < 32 || characterInput > 90 || encodingInput == null || encodingInput.length() < 1){ // checks if ASCII codes between 32 and 90 and an encoding value that cannot be NULL and of length at least one.
+            throw new Exception ("The character " + characterInput + " is not supported Morse character");
         }
         character = characterInput;
         encoding = encodingInput;
@@ -51,10 +56,10 @@ public class MorseCode{
      *Setter method to put in encoding of user input
      *changes encoding object of construtor to that of the users input
      */
-    public void setEncoding(string value){ //setter for encoding
+    public void setEncoding(String value){ //setter for encoding
         this.encoding = value;
     }
 
     //sets up character and encoding
 }
-//output of the jar file should be what your code look like
+//NOTE TO SELF output of the jar file should be what your code look like
--- AssignmentThree/MorseCodeConvert.java
+++ AssignmentThree/MorseCodeConvert.java
@@ -5,10 +5,10 @@
 *@version 1.0   3/26/2021
 */
 
-import java.util.ArrayList;
+import java.util.ArrayList;//ArrayList for storing characters and morse code
 import java.util.Scanner; //Scanner for user input
 import java.io.File; //Gives File Class
-import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;//Gives FileNotFoundException
 
 /**
  * Class that stores information given user input from TestPersonInfo
@@ -19,29 +19,42 @@ public class MorseCodeConvert {
     //2) reads the file
     //3) setup the lookup table (listcode)
         //morsecode objects to refer to each character
-    private ArrayList <MorseCode> listCodes = new ArrayList <MorseCode>();
+    private ArrayList <MorseCode> listCodes = new ArrayList <MorseCode>();// creates ArrayList that stores the characters and its morse code conversions
     
-    
-
-    public MorseCodeConvert(String value){
+    /**
+     * This is the non-default constructor.
+     * It reads all the lines from the file and add each valid line to ArrayList. Will throw expception message for skipped lines
+     * @param value this is the file to be read
+     * @throws FileNotFoundException an Exception that will be thrown if file used for this method is not found.
+     * @throws Exception an Exception that will be thrown if 
+     */
+    public MorseCodeConvert(String value) throws FileNotFoundException, Exception{
         Scanner scanner;
-        try{
+        try{//if file is not found, throw exception stating the file is not found
             scanner = new Scanner(new File(value));
         }
         catch(FileNotFoundException issue){
-            throw Exception ("Failed to open file: " + value);
+            System.err.println("Failed to open file: " + value);
+            throw issue;
         }
     
-        while(scanner.hasNextline()){
+        while(scanner.hasNextLine()){//checks if there is another line in scammer
             String readLine = scanner.nextLine();// reads line
     
-            String[] tabs = sentence.split("\t"); // tabs will contain two parts, one with characters, one with morse code
+            String[] tabs = readLine.split("\t"); // tabs will contain two parts, one with characters, one with morse code
     
-            if (readline.matches(".+\\t+\\W")){
-                listCodes.add(new MorseCode(tabs[0].charAt(0),tabs[1]));
+            if (readLine.matches(".+\t+\\W+")){ //regex to check the format of the given morse code converstion
+                try
+                {
+                    listCodes.add(new MorseCode(tabs[0].charAt(0),tabs[1]));//adds to default constructor
+                }
+                catch (Exception e)
+                {
+                    System.err.println(e);
+                }
             }
             else{
-                System.err.println("Invalid line: " + readLine);
+                System.err.println("Invalid line: " + readLine);//if there is an invalid line, it will state it
             }
         }
         scanner.close();
@@ -52,7 +65,7 @@ public class MorseCodeConvert {
      */
     public void printEncodingList(){
         for(int h = 0; h < listCodes.size(); h++){
-            System.out.println(listCodes.get(h).getCharacter() +"\t" + listCodes.get(h).getEncoding());
+            System.out.println("('" + listCodes.get(h).getCharacter() +"', " + listCodes.get(h).getEncoding()+")");
         }
     }
 
@@ -61,54 +74,82 @@ public class MorseCodeConvert {
      * @param value a String that the user wants to convert to morse code
      */
     public void encodeString(String value){
-        String morseEquivalent;
+        String morseEquivalent = "";
+        String space = " ";
 
+        if(value == null || value == "") //if there is no string, print nothing and skip line
+        {
+            System.out.println();
+            return;
+        }
         for(int i = 0; i < value.length(); i++){
-            Char currentChar = value.charAt(i);
+            char currentChar = Character.toUpperCase(value.charAt(i));//turns lowercase to uppercase
             int j = 0;
             boolean found = false;
-            while (j < listCodes.size()){
-                if(currentChar != listCodes.get(j).getCharacter()) found = true;
-                j++;
+            while (j < listCodes.size()){// checks character with the ones in listcode
+                if(currentChar == listCodes.get(j).getCharacter())  //if the character is found, turn found to true break the loop
+                {
+                    found = true;
+                    break;
+                }
+                j++;//moves on to next character
             }
-            if(found){
-                morseEquivalent = morseEquivalent + listCodes.get(j).getEncoding();
+            if(found){//if character is found, add the morse code equivalent to the morse code converstion and a space
+                morseEquivalent = morseEquivalent + listCodes.get(j).getEncoding() + space;
+            }
+            else if(currentChar == ' '){// this was the original idea of adding a space but it doesnt seem to work. If current character is a space, add a space to morse code
+            morseEquivalent = morseEquivalent + ' ';
             }
         }
         System.out.println(morseEquivalent);
     }
 
     /**
-     * This is the method for turning a String into morse code
+     * This is the method for turning a text file into morse code
      * @param value a String that the user wants to convert to morse code
+     * @throws FileNotFoundException an exception that is thrown if file cannot be opened
      */
-    public void encodeFile(String value){
+    public void encodeFile(String value) throws FileNotFoundException{
+        String space = " ";
+        String morseEquivalent = "";//starts morseEquivalent with nothing in String
         Scanner scanner;
         try{ //attempts to open the file, if it failes, an exception is thrown.
             scanner = new Scanner(new File(value));
         }
         catch(FileNotFoundException issue){
-            throw Exception ("Failed to open file: " + value);
+            System.err.println("Failed to open file: " + value);
+            throw issue;
         }
 
-        while(scanner.hasNextline()){
-            for(int i = 0; i < value.length(); i++){
-                Char currentChar = value.charAt(i);
+        String readline;
+        while(scanner.hasNextLine()){//if scanner has a next line, continue the loop
+            readline = scanner.nextLine();
+            for(int i = 0; i < readline.length(); i++){//loop to convert string character to morse code
+                char currentChar = Character.toUpperCase(readline.charAt(i));//makes characters capitalized
                 int j = 0;
                 boolean found = false;
-                while (j < listCodes.size()){
-                    if(currentChar != listCodes.get(j).getCharacter()) found = true;
-                    j++;
+                while (j < listCodes.size()){// checks character with the ones in listcode
+                    if(currentChar == listCodes.get(j).getCharacter()) //if the character is found, break the loop
+                    {
+                        found = true;
+                        break;
+                    }
+                    j++;//moves on to next character
                 }
                 if(found){
-                    morseEquivalent = morseEquivalent + listCodes.get(j).getEncoding();
+                    morseEquivalent = morseEquivalent + listCodes.get(j).getEncoding()+ space;
+                    // morseEquivalent = morseEquivalent + listCodes.get(j).getCharacter();
                 }
-                else
+                else if(currentChar == ' '){ //if there is space, add a space in morsecode
+                    morseEquivalent = morseEquivalent + ' ';
+                }
+                else//if there is an unidentified value, add a ?
                 {
                     morseEquivalent = morseEquivalent + "?";
                 }
+                
             }
-            System.out.println(morseEquivalent);
+            System.out.println(morseEquivalent);//print morse code equivalent
         }
         scanner.close();
     }
